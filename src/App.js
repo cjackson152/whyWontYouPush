6aeed3244ae583b5fd7af8f8cd3b837e5967b920
cole jackson <cole_jackson@live.com>
2020-08-11 10:59:24 -0700

    why do i struggle so

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react'
 import SearchResult from "./components/SearchResult";
 import "./App.css"
-const express = require('exrpess')
-const app = express();
-const path = require('path');
-const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, "..", 'public');
-app.use(express.static(publicPath));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
-  
-})
+
 
 function App() {
   return <SearchResult/>
 }
 
-app.listen(port, () => {
-  console.log(`connected ${port}`)
-})
+
+
 export default App;
