219765ebaaa0051d65c20eaf00878528661d91b5
cole jackson <cole_jackson@live.com>
2020-08-11 10:18:33 -0700

    fixes forheroku

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import SearchResult from "./components/SearchResult";
 import "./App.css"
+const express = require('exrpess')
+const app = express();
+const path = require('path');
+const publicPath = path.join(__dirname, "..", 'public');
+app.use(express.static(publicPath));
+
+
 function App() {
   return <SearchResult/>
 }
