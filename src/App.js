350b32abd57ac91b607190a09d757e1d20470ec0
cole jackson <cole_jackson@live.com>
2020-08-11 10:24:11 -0700

    more fixes woo

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import "./App.css"
 const express = require('exrpess')
 const app = express();
 const path = require('path');
+const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, "..", 'public');
 app.use(express.static(publicPath));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+  
+})
 
 function App() {
   return <SearchResult/>
 }
+
+app.listen(port, () => {
+  console.log(`connected ${port}`)
+})
 export default App;
